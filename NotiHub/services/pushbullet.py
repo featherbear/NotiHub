e64608266f7cff2422939ae88263e39ea2a43df1
bearbear12345 <andrew.j.wong@outlook.com>
2017-08-06 20:52:31 +1000

    Working pushbullet.py script
    Will do threading later

--- NotiHub/services/pushbullet.py
+++ NotiHub/services/pushbullet.py
@@ -11,50 +11,48 @@ The following code is licensed under the GNU Public License Version v3.0
 __VERSION__ = "0.0.1"
 import pushbullet
 
+pushbullet.Pushbullet._push_ = pushbullet.Pushbullet._push
+pushbullet.Pushbullet._push = lambda self, data: self._push_(
+    {**data, "source_device_iden": self.device_iden} if hasattr(self, "device_iden") else data)
+
 
 class service():
-    def __init__(self, api_key, *, send, receive, device_iden=None):
+    def __init__(self, api_key, *, send, receive):
         self.canSend = send
         self.canReceive = receive
         self.pushbullet = pushbullet.Pushbullet(api_key)
-        self.listener = pushbullet.Listener(self.pushbullet, self._eventPush)
-        self.device = None
-        if device_iden:
-            results = [d for d in self.pb.devices if d.device_iden == device_iden and d.active]
-            self.device = results[0] if results else None
-        self.last_push = 0
+        self.listener = pushbullet.Listener(self.pushbullet,
+                                            lambda event: (self._pushFetch() if event["type"] == "tickle" else None))
 
+        self.device = ([d for d in self.pushbullet.devices if d.nickname == "NotiHub"] + [None])[0]
         if not self.device:
             try:
-                device = self.pb.new_device("NotiHub")
-                print("Created new device: NotiHub (id: %s)" % device.device_iden)
-                self.device = device
+                self.device = self.pushbullet.new_device("NotiHub")
+                print("Created new device: NotiHub (id: %s)" % self.device.device_iden)
             except:
                 print("Error: Unable to create device")
                 raise
 
-    def _pushEvent(self, event):
-        print(event)
-        if event["type"] == "push" and event["push"]["type"] == "mirror":
-            print("MIRROR")
-        elif event["type"] == "tickle":
-            print("TICKLE")
-            self.check_pushes()
+        self.last_push = 0
 
     def _pushFetch(self):
         pushes = self.pushbullet.get_pushes(self.last_push)
         for push in pushes:
             if not isinstance(push, dict):
                 continue
+
+            # Get only pushes directed to the NotiHub device - Do we want to keep this function?
             if ((push.get("target_device_iden", self.device.device_iden) == self.device.device_iden) and not (
                     push.get("dismissed", True))):
-                self.handler(push.get("title", ""), push.get("body", ""))
+                self.handler(push.get("created"), push.get("title", None), push.get("body", None))
                 self.pushbullet.dismiss_push(push.get("iden"))
+
             self.last_push = max(self.last_push, push.get("created"))
 
-    def send(self, data, title="", device=None):
+    def send(self, data, title="NotiHub", device=None):
         if self.canSend:
-            return self.pushbullet.push_note(title, data, device=device)
+            return self.pushbullet.push_note(title, data, device=(
+            type('s', (), {"device_iden": device}) if type(device) == str else device))
 
     def listen(self):
         if self.canReceive:
@@ -63,6 +61,5 @@ class service():
     def stopListen(self):
         self.listener.close()
 
-    def handler(self, title, body):
-        print(title)
-        print(body)
+    def handler(self, ts, title, body):
+        pass
