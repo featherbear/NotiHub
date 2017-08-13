4882dee92a47d35bc0222360130a3147adab6648
bearbear12345 <andrew.j.wong@outlook.com>
2017-08-13 22:14:15 +1000

    ngh

--- NotiHub/grabbers/tripview.py
+++ NotiHub/grabbers/tripview.py
@@ -25,3 +25,167 @@ class service():
 
     def stopListen(self):
         pass
+
+
+from datetime import datetime, time
+
+import requests
+
+"""
+
+https://api.tripview.com.au/config	GET	200	610b	215ms
+https://api.tripview.com.au/svinfo?region=syd&version=3.5.6	GET	200	8.6kb	356ms
+https://static.tripview.com.au/syd-401.zip	GET	200	1.8mb	3s
+https://api.tripview.com.au/realtime?region=syd&routes=CR_eh_d&type=dtva	GET	200	921b	16s
+https://api.tripview.com.au/realtime?region=syd&routes=CR_eh_u&type=dtva	GET	200	765b	22s
+https://api.tripview.com.au/realtime?region=syd&routes=SB_M41_u&type=dtva	GET	200	135b	36s
+https://api.tripview.com.au/realtime?region=syd&routes=SB_M41_d&type=dtva	GET	200	135b	36s
+https://api.tripview.com.au/realtime?region=syd&routes=SB_491_d&type=dtva	GET	200	135b	51s
+https://api.tripview.com.au/realtime?region=syd&routes=SB_491_u&type=dtva	GET	200	135b	52s
+https://api.tripview.com.au/realtime?region=syd&routes=CR_eh_u%2CCR_il_d%2CCR_sc_d%2CCR_eh_d&type=dtva	GET	200	1.2kb	58s
+https://api.tripview.com.au/realtime?region=syd&routes=CR_sc_u%2CCR_eh_d%2CCR_il_u&type=dtva	GET	200	688b	1min
+
+"""
+
+ # HEAD https://api.transport.nsw.gov.au/v1/publictransport/timetables/complete/gtfs
+
+import peewee
+
+nswtransport_db = peewee.SqliteDatabase("nswtransport.db")
+
+
+class GTFSModel(peewee.Model):
+    class Meta:
+        database = nswtransport_db
+
+
+class Agency(GTFSModel):
+    # "X0000","NSW TrainLink","http://transportnsw.info","Australia/Sydney","EN","131500"
+    agencyID = peewee.CharField(unique=True)
+    agencyName = peewee.CharField()
+
+
+class Calendar(GTFSModel):
+    # "TA+r1359+1","0","0","0","1","1","0","0","20170810","20171108"
+    serviceID = peewee.CharField(unique=True)
+    monday = peewee.BooleanField()
+    tuesday = peewee.BooleanField()
+    wednesday = peewee.BooleanField()
+    thursday = peewee.BooleanField()
+    friday = peewee.BooleanField()
+    saturday = peewee.BooleanField()
+    sunday = peewee.BooleanField()
+    datestart = peewee.DateField()
+    dateend = peewee.DateField()
+
+
+class CalendarDate(GTFSModel):
+    # "TA+r1034+1","20170810","2"
+    # ???
+    serviceID = peewee.ForeignKeyField(Calendar, related_name="dates")
+    date = peewee.DateField()
+    exceptionType = peewee.IntegerField()
+
+
+class Route(GTFSModel):
+    # "2-BMT-sj2-1","X0000","","Blue Mountains Line","Intercity Trains Network","2","F6891F","FFFFFF"
+    routeID = peewee.CharField(unique=True)
+    agency = peewee.ForeignKeyField(Agency, related_name="routes")
+    shortName = peewee.CharField()
+    longName = peewee.CharField()
+    description = peewee.CharField()
+    type = peewee.CharField()
+
+
+class Trip(GTFSModel):
+    # "2-BMT-sj2-1","TA+r1034+1","2.TA.2-BMT-sj2-1.1.H","2-BMT-sj2-1.1.H","Springwood Station","0","1+2","1"
+    tripID = peewee.CharField()
+    routeID = peewee.ForeignKeyField(Route, related_name="trips")
+    serviceID = peewee.ForeignKeyField(Calendar, related_name="trips")
+    headsign = peewee.CharField()
+    direction = peewee.BooleanField()
+    blockID = peewee.CharField()
+
+
+class Stop(GTFSModel):
+    # "228055","228055","Pacific Hwy After Murray St","-33.0036096083102","151.685565693411","","","0",""
+    stopID = peewee.CharField(unique=True)
+    name = peewee.CharField()
+    platform = peewee.CharField()
+
+
+class StopTime(GTFSModel):
+    # "1.TA.2-BMT-sj2-1.1.H", "07:52:01", "07:52:01", "2000338", "1", "", "0", "1", "0"
+    tripID = peewee.ForeignKeyField(Calendar, related_name="times")
+    stopID = peewee.ForeignKeyField(Stop, related_name="times")
+    time = peewee.TimeField()
+models = [Agency, Calendar, CalendarDate, Route, Trip, Stop, StopTime]
+
+nswtransport_db.connect()
+
+
+#
+import csv
+#
+
+#
+#
+print("Checking for updated timetable...")
+with requests.get("https://api.transport.nsw.gov.au/v1/publictransport/timetables/complete/gtfs", headers={"Authorization": "apikey WUQ7i7CBYCQmlAf4uh8I5L7pjS9Zq5uZbNNF"}, stream=True) as gtfsSource:
+    sourcetime = gtfsSource.headers["Last-Modified"]
+    sourcetimestamp = int(datetime.strptime(sourcetime, '%a, %d %b %Y %H:%M:%S %Z').timestamp())
+    if sourcetimestamp > nswtransport_db.pragma("user_version")[0]:
+        print("New version: " + sourcetime)
+        import tempfile, os
+        # with tempfile.TemporaryDirectory() as tempdir:
+        tempdir = "D:\\LOL"
+        gtfsZip = os.path.join(tempdir, "gtfs.zip")
+        # with open(gtfsZip, 'wb') as f:
+        #     print("Downloading")
+        #     for chunk in gtfsSource.iter_content(chunk_size=1024):
+        #         if chunk:  # filter out keep-alive new chunks
+        #             f.write(chunk)
+        print("Extracting")
+        import zipfile
+        with zipfile.ZipFile(gtfsZip, "r") as z:
+            z.extractall(tempdir)
+        class gtfsFile(object):
+            def __init__(self,filename):
+                self.csv = csv.reader(open(os.path.join(tempdir,filename+".txt")), delimiter=',')
+                next(self.csv,None)
+            def __enter__(self):
+                return self.csv
+            def __exit__(self,*_):
+                pass
+        print("Dropping and creating tables")
+        nswtransport_db.drop_tables(models, safe=True)
+        nswtransport_db.create_tables(models, safe=True)
+        print("Reading GTFS files")
+        with gtfsFile("agency") as agency, gtfsFile("calendar") as calendar, gtfsFile("calendar_dates") as calendar_dates, gtfsFile("routes") as routes, gtfsFile("stop_times") as stop_times, gtfsFile("stops") as stops, gtfsFile("trips") as trips:
+            #print("Populating agencies")
+            #[Agency.create(agencyID=row[0],agencyName=row[1]) for row in agency]
+            print("Populating services")
+            [Calendar.create(*row[:9]) for row in calendar]
+            [CalendarDate.create(*row) for row in calendar_dates]
+            [Route.create(*row[:6]) for row in routes]
+            [Stop.create(row[0],row[2],row[-1]) for row in stops]
+            [Trip.create(row[2],*row[:2],*row[4:7]) for row in trips]
+            print("Populating stop times")
+            [StopTime.create(row[0],row[3],row[1]) for row in stop_times]
+
+        nswtransport_db.pragma("user_version", sourcetimestamp)
+        print("Timetable information updated")
+# for o in requests.get("http://realtime.grofsoft.com/tripview/realtime?routes=SB_M41_u&type=dv").json()['delays']:
+#     o = o['offsets'].split(",")
+#     l = {}
+#     t = datetime.now()
+#     T = [int(i) for i in o[-2].split(":")]
+#     if time(*T) >= time(t.hour, t.minute):
+#         print("Already passed")
+#     else:
+#         for a in range(0, len(o), 2):
+#             l[o[a]] = o[a + 1]
+#         print(l)
+
+
+# need to get stop times
