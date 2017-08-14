16664fa070737ff2bf3b86a69d7c93acac6dc1d4
bearbear12345 <andrew.j.wong@outlook.com>
2017-08-15 00:13:45 +1000

    Start working on RSS and weather
    Lol 3u maths today, will be fun!?!!?

--- NotiHub/grabbers/rss.py
+++ NotiHub/grabbers/rss.py
@@ -9,13 +9,15 @@ The following code is licensed under the GNU Public License Version v3.0
 """
 
 __VERSION__ = "0.0.1"
+from .__stub__ import Grabber
 
+import feedparser
+class grabber(Grabber):
+    def __init__(self, feed=[], *feeds):
+        self.urls = feed + [*feeds]
 
-class service():
-    def __init__(self, login, *, send, receive):
-        self.canSend = send
-        self.canReceive = receive
-        pass
+    def aggregate(self):
+        self.feed = [None if "bozo_exception" in feed else feed for feed in [feedparser.parse(feed) for feed in self.urls]]
 
     def send(self, thread, data):
         pass
